a5bf1295d740287a18fd5c75b9234282dd589270
xuping <13289341606@163.com>
2020-03-07 23:20:26 +0800

    我

--- app.js
+++ app.js
@@ -30,13 +30,10 @@ App({
           this.globalData.loginStatus = 1;
           wx.getUserInfo({
             success: res => {
-              // console.log(res);
+               console.log(res);
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
-// 发送unionId给后台
               this.globalData.myInfoOperation = "已完善";
-
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
